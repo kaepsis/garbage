075c095046c25d721bdbbb541b93151e95618c13
Lorenzo Durastante <durastante.lorenzo02@gmail.com>
2022-09-15 10:35:02 +0200

    Update ts3installer.sh

--- ts3installer.sh
+++ ts3installer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TS3URL="https://files.teamspeak-services.com/releases/server/3.13.6/teamspeak3-server_linux_amd64-3.13.6.tar.bz2"
+TS3URL="https://files.teamspeak-services.com/releases/server/3.13.7/teamspeak3-server_linux_amd64-3.13.7.tar.bz2"
 FILENAMEWITHEXT="${TS3URL##*/}"
 FILENAME="${FILENAMEWITHEXT%-*}"
 
