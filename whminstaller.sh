5311f6d9268edbfe2bfbd4844a97148c89ad3e5a
Lorenzo Durastante <durastante.lorenzo02@gmail.com>
2022-09-23 20:29:39 +0200

    Update whminstaller.sh

--- whminstaller.sh
+++ whminstaller.sh
@@ -1,5 +1,9 @@
+DOT_IP=curl https://icanhazip.com
+DASH_IP=curl http://139.162.137.11/ipconv.php?ip=$DOT_ip
 sudo yum update -y
 sudo yum upgrade -y
 sudo cd /home
 sudo curl -o latest -L https://securedownloads.cpanel.net/latest
-sudo sh latest
+HALF_URL=sudo sh latest | grep -oP -m 1 '(?=cprapid).*'
+FULL_URL="https://$DASH_IP.$HALF_URL"
+echo $FULL_URL
