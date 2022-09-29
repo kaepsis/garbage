741cd2b9b6b0b2f82a544530b82021669070c410
Lorenzo Durastante <durastante.lorenzo02@gmail.com>
2022-09-29 13:14:51 +0200

    Update whminstaller.sh

--- whminstaller.sh
+++ whminstaller.sh
@@ -1,28 +1,13 @@
-echo "#########################"
-echo "# Retrieving machine IP #"
-echo "#########################"
 DOT_IP=$(curl https://icanhazip.com)
 DASH_IP=$(curl http://139.162.137.11/ipconv.php?ip=$DOT_IP)
 clear
-echo "##########################"
-echo "#   Installing updates   #"
-echo "##########################"
 sudo yum update -y
 clear
-echo "###########################"
-echo "#   Installing upgrades   #"
-echo "###########################"
 sudo yum upgrade -y
 clear
-echo "#########################"
-echo "#   Installing CPanel   #"
-echo "#########################"
 sudo cd /home
 sudo curl -o latest -L https://securedownloads.cpanel.net/latest
 HALF_URL=sudo sh latest | grep -oP -m 1 '(?=cprapid).*'
 FULL_URL="https://$DASH_IP.$HALF_URL"
 clear
-echo "############################"
-echo "#   Link to CPanel Setup   #"
-echo "############################"
 echo $FULL_URL
